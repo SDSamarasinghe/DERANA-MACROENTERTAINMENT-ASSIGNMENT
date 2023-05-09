85c96a66e9c463a443a76b53d1e3c66aff6b03c6
lksad <it20605902@my.sliit.lk>
2023-05-09 16:05:44 +0530

    initial commit

---
+++ client/src/components/account/Login.jsx
@@ -0,0 +1,27 @@
+import { Box, TextField, Button, styled } from '@mui/material';
+
+const Component = styled(Box)`
+ width:400px;
+ margin: auto;
+ box-shadow: 5px 2px 5px 2px rgb(0 0 0/ 0.6);
+`;
+
+const Image = styled('img')({
+    width: 100
+})
+
+const Login = () => {
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+
+    return (
+        <Component>
+            <Image src={imageURL} alt="Login" />
+            <TextField variant="standard"/>
+            <TextField variant="standard"/>
+            <Button variant="contained">Login</Button>
+            <Button>Create an account</Button>
+        </Component>
+    )
+}
+
+export default Login;
